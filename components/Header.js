1b23d0f585fdda4f2d24e06095b1a184f19eb8b0
Aditya Bhattad <adityabhattad18@gmail.com>
2022-11-16 17:55:19 +0530

    Added Link to Manage-Voter page

--- components/Header.js
+++ components/Header.js
@@ -1,7 +1,13 @@
 import { ConnectButton } from "@rainbow-me/rainbowkit";
 import Link from "next/link";
+import { useIsMounted } from "../hooks/useIsMounted";
+import { useIsOrganiser } from "../hooks/useIsOrganiser";
 
 export default function Header() {
+
+	const isOrganiser = useIsOrganiser();
+	const mounted = useIsMounted();
+
 	return (
 		<nav className="p-5 border-b-2 flex flex-row justify-between items-center">
 			<div className="flex flex-row items-center">
@@ -10,11 +16,20 @@ export default function Header() {
 						Decentralized Voting
 					</h1>
 				</Link>
-				<Link href="/find-winner">
-					<button className="px-3 py-2  bg-black text-white rounded-xl text-md font-bold ">
+				<Link href="/find-winner" >
+					<button className="px-3 py-2  bg-black text-white rounded-xl text-md font-bold">
 						Find Winner
 					</button>
 				</Link>
+				{
+					mounted && isOrganiser && (
+						<Link href="/create-election/voters-list">
+							<button className="px-3 py-2  bg-black text-white rounded-xl text-md font-bold mx-2">
+								Manage Voters
+							</button>
+						</Link>
+					)
+				}
 			</div>
 			<ConnectButton
 				showBalance={true}
