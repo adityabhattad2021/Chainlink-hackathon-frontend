91ab0949f518b1fc1c8ae2149ac71dab03034f0a
Aditya Bhattad <adityabhattad18@gmail.com>
2022-11-16 17:45:27 +0530

    Add Route For Find Voter Page

--- components/Header.js
+++ components/Header.js
@@ -4,11 +4,18 @@ import Link from "next/link";
 export default function Header() {
 	return (
 		<nav className="p-5 border-b-2 flex flex-row justify-between items-center">
-			<Link href="/">
-				<h1 className="py-4 px-4 font-bold text-3xl">
-					Decentralized Voting
-				</h1>
-			</Link>
+			<div className="flex flex-row items-center">
+				<Link href="/">
+					<h1 className="py-4 px-4 font-bold text-3xl">
+						Decentralized Voting
+					</h1>
+				</Link>
+				<Link href="/find-winner">
+					<button className="px-3 py-2  bg-black text-white rounded-xl text-md font-bold ">
+						Find Winner
+					</button>
+				</Link>
+			</div>
 			<ConnectButton
 				showBalance={true}
 				accountStatus={"full"}
